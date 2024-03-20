36aaceb57f2b30d92f501b016ef92d18966a1e85
James Charters <me@jamescharters.info>
2024-03-20 19:01:32 +1100

    chore: add sensible property initialisers

--- MilkrunApi/Models/ProductRequest.cs
+++ MilkrunApi/Models/ProductRequest.cs
@@ -28,27 +28,27 @@ public record ProductRequest : IProduct
     [Range(1, int.MaxValue, ErrorMessage = "Price must be greater than zero.")]
     [Required(ErrorMessage = "Price is required.")]
     [RegularExpression("^(0|[1-9]\\d*)$", ErrorMessage = "Price must be integer >= 0.")]
-    public required int Price { get; set; }
+    public required int Price { get; set; };
 
     /// <summary>
     ///     Discount percentage of the product, if applicable
     /// </summary>
     [Range(0, 100, ErrorMessage = "Discount percentage must be between 0 and 100.")]
     [RegularExpression("^\\d+(\\.\\d{1,2})?$", ErrorMessage = "Discount percentage must be decimal up to 2 places.")]
-    public decimal? DiscountPercentage { get; set; }
+    public decimal? DiscountPercentage { get; set; } = 0;
 
     /// <summary>
     ///     Rating of the product, if applicable
     /// </summary>
     [Range(0, 5, ErrorMessage = "Rating must be between 0 and 5.")]
     [RegularExpression("^\\d+(\\.\\d{1,2})?$", ErrorMessage = "Rating must be decimal up to 2 places.")]
-    public decimal? Rating { get; set; }
+    public decimal? Rating { get; set; } = 0;
 
     /// <summary>
     ///     Quantity in stock, if applicable
     /// </summary>
     [RegularExpression("^(0|[1-9]\\d*)$", ErrorMessage = "Stock must be integer >= 0.")]
-    public int? Stock { get; set; }
+    public int? Stock { get; set; } = 0;
 
     /// <summary>
     ///     Brand name, if applicable
@@ -68,5 +68,5 @@ public record ProductRequest : IProduct
     /// <summary>
     ///     Detailed product images, if applicable
     /// </summary>
-    public IList<string>? Images { get; set; }
+    public IList<string>? Images { get; set; } = new List<string>();
 }
